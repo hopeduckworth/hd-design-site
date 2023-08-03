6ae54563c61d6b8f0cb023fed6b9d8735cc17102
Ian Duckworth <ianleeduckworth@gmail.com>
2023-08-02 22:01:44 -0400

    updated input component to have some basic styling

--- src/design-system/atoms/Input.tsx
+++ src/design-system/atoms/Input.tsx
@@ -2,13 +2,19 @@ import { InputHTMLAttributes } from "react";
 import styled from "styled-components";
 
 const StyledInput = styled.input`
-   color:orange;
+  color: white;
+  border: 2px solid gray;
+  background-color: #d7b6d7;
+  padding: 0.25rem 0.5rem;
+  outline: none;
+
+  &:focus {
+    border: 2px solid lightgray;
+  }
 `;
 
-export type InputProps = InputHTMLAttributes<HTMLInputElement> & {
-}
-
+export type InputProps = InputHTMLAttributes<HTMLInputElement> & {};
 
 export const Input = (props: InputProps) => {
-    return <StyledInput {...props} ></StyledInput>
-}
+  return <StyledInput {...props}></StyledInput>;
+};
